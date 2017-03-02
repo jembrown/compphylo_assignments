ae4239dfe6e0b66b630ed817d8123a96b3b73ca8
z.a.c <zcarver@agcenter.lsu.edu>
2017-03-02 11:10:37 -0600

    Update MCMC_a3.py

--- MCMC_a3.py
+++ MCMC_a3.py
@@ -62,10 +62,37 @@ def acc_rej(curr_post, new_post):
             return post
             #return state
 
-def plots(param_vals, likelihoods, posteriors):
+def plots(priors, param_vals, likelihoods, posteriors):
+	'''Traceplots of the parameters, likelihoods and posteriors.'''
+	fig,ax=plt.subplots()
+	ax.plot(priors)
+	x=ax.set(xlabel='Samples',ylabel='priors')
+	plt.show()
+
+	fig,ax=plt.subplots()
+	ax.plot(param_vals)
+	x=ax.set(xlabel='Samples',ylabel='parameters')
+	plt.show()
+
+	fig,ax=plt.subplots()
+	ax.plot(likelihoods)
+	x=ax.set(xlabel='Samples',ylabel='likelihoods')
+	plt.show()
+
+	fig,ax=plt.subplots()
+	ax.plot(posteriors)
+	x=ax.set(xlabel='Samples',ylabel='posteriors')
+	plt.show()
+	
 	'''Histograms of the parameter values (priors), likelihoods and posteriors.'''
-	plt.hist(param_vals)
+	plt.hist(priors)
 	plt.title("Priors")
+	plt.xlabel("priors")
+	plt.ylabel("Frequency")
+	plt.show()
+	
+	plt.hist(param_vals)
+	plt.title("Parameters")
 	plt.xlabel("param_vals")
 	plt.ylabel("Frequency")
 	plt.show()
@@ -93,19 +120,20 @@ def MCMC():
 	prr = prior(p)
 	curr_param = draw_param(n, p)
 	curr_post = posterior_UN(prr, lk)
+	priors = []
 	param_vals = []#define a list to hold the chained parameter values from the Markov chain.
-    likelihoods =  []#define a list to hold the found likelihoods
-    posteriors = []#define a list to hold the posteriors 
-    '''chain'''
-	for val in range(100):#go through an x number of replicates, here 100
-		new_param = draw_param(n, p)#each iteration draw a new and random parameter in n trials with a probability of p
-	    #normp = np.random.triangular(0,1,1)
-	    likelihoods.append(like(new_param, n, p))
-		new_post = posterior_UN(prr, like(new_param, n, p))
-		posteriors.append(posterior_UN(prr, like(new_param, n, p)))
-		param_vals.append(acc_rej(curr_post, new_post))
-	print('yes', param_vals)
-	plots(param_vals, likelihoods, posteriors)#now plot the histograms.
+        likelihoods =  []#define a list to hold the found likelihoods
+        posteriors = []#define a list to hold the posteriors 
+        '''chain'''
+        for val in range(100):#go through an x number of replicates, here 100
+	        new_param = draw_param(n, p)#each iteration draw a new and random parameter in n trials with a probability of p
+	        #normp = np.random.triangular(0,1,1)
+	        likelihoods.append(like(new_param, n, p))
+	        new_post = posterior_UN(prr, like(new_param, n, p))
+	        posteriors.append(posterior_UN(prr, like(new_param, n, p)))
+	        param_vals.append(acc_rej(curr_post, new_post))
+                print('parameter values', param_vals)
+	plots(priors.append(prr), param_vals, likelihoods, posteriors)#now plot the histograms.
 	
 
 if __name__ == '__main__':
